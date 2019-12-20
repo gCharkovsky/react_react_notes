a61457f9cf8d17a74cdfc11cc6a5c7b2430112f9
gCharkovsky <g.charkovsky@gmail.com>
2019-12-20 20:12:19 +0300

    Кто-то сказал "дедлайн"?

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/reset.css';
+import './styles/basic_theme.css';
+import './styles/themes/dark.css';
+import NotesBrowser from "./views/NotesBrowser/NotesBrowser";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NotesBrowser/>
+        </div>
+    );
 }
 
 export default App;
